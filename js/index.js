3985a42d8d9c216a5752a22222f38b1d09ce5703
Okeogbe <dharniell007@gmail.com>
2021-08-23 07:08:25 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 $(".menuIcon").on("click", () => {
-    $(".nav__mobileMenu").addClass('active') 
+    $(".nav__mobileMenu").addClass('active')
 })
 
 $('.close').on('click', () => {
     $('.nav__mobileMenu').removeClass('active')
+})
+
+$(window).on('scroll', () => {
+    if ($(window).scrollTop() > 200) {
+        $('.nav').addClass('fixed')
+    } else {
+        $('.nav').removeClass('fixed');
+    }
 })
