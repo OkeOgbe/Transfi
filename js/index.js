46105b6c5ab40ec681f29597ce9faceef2df539f
Okeogbe <dharniell007@gmail.com>
2021-09-17 13:13:43 +0100

    update

--- js/index.js
+++ js/index.js
@@ -2,7 +2,19 @@ $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
       autoplay: true,
       loop: true,
-      items: 3
+      items: 3,
+      center: true,
+          responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
       });
 });
 
